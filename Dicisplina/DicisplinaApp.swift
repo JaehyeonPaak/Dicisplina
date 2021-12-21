8b0e6844ff50a6a0332c626bd43d175fe46d5f77
JaehyeonPaak <wogus392@gmail.com>
2021-12-21 17:47:41 +0900

    Initial Commit

---
+++ Dicisplina/DicisplinaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DicisplinaApp.swift
+//  Dicisplina
+//
+//  Created by United States on 12/21/21.
+//
+
+import SwiftUI
+
+@main
+struct DicisplinaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
