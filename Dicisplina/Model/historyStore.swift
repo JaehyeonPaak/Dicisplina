21336350d42bca344e30e4c7fb09f5730489c092
Jaehyeon Park <91472722+JaehyeonPaak@users.noreply.github.com>
2022-01-11 22:37:05 +0900

    Update historyStore.swift

--- Dicisplina/Model/historyStore.swift
+++ Dicisplina/Model/historyStore.swift
@@ -1,1 +1,61 @@
 
+//
+//  HistoryStore.swift
+//  Dicisplina
+//
+//  Created by United States on 12/27/21.
+//
+import Foundation
+
+struct ExerciseDay: Identifiable {
+    let id = UUID()
+    let date: Date
+    var exercises: [String] = []
+}
+
+class HistoryStore: ObservableObject {
+    
+    @Published var exerciseDays: [ExerciseDay] = []
+    
+    init() {}
+    
+    init(withChecking: Bool) throws {
+        do {
+            try load()
+        } catch {
+            throw error
+        }
+    }
+    
+    func addDoneExercise(_ exerciseName: String) {
+        let today = Date()
+        if let firstDate = exerciseDays.first?.date {
+            if isSameDay(date1: today, date2: firstDate) {
+                exerciseDays[0].exercises.append(exerciseName)
+            }
+        }
+        else {
+            exerciseDays.insert(ExerciseDay(date: today, exercises: [exerciseName]), at: 0)
+        }
+        print("History: ", exerciseDays)
+    }
+    
+    func isSameDay(date1: Date, date2: Date) -> Bool {
+        let diff = Calendar.current.dateComponents([.day], from: date1, to: date2)
+        if diff.day == 0 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    enum FileError: Error {
+        case loadFailure
+        case saveFailure
+        case urlFailure
+    }
+    
+    func load() throws {
+        throw FileError.loadFailure
+    }
+}
